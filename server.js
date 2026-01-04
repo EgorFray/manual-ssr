7e61cd9bb760dd584b17538009a34b3a1865a749
EgorFray <fatamorganaa933@gmail.com>
2026-01-04 12:34:12 +0200

    Add dumb html file to the server

--- server.js
+++ server.js
@@ -1,10 +1,84 @@
+const { readFileSync } = require("fs");
 const { createServer } = require("http");
-const { parse } = require("path");
+const { parse } = require("url");
+const { renderToString } = require("react-dom/server");
+const React = require("react");
+
+const pizzas = [
+	{
+		name: "Focaccia",
+		price: 6,
+	},
+	{
+		name: "Pizza Margherita",
+		price: 10,
+	},
+	{
+		name: "Pizza Spinaci",
+		price: 12,
+	},
+	{
+		name: "Pizza Funghi",
+		price: 12,
+	},
+	{
+		name: "Pizza Prosciutto",
+		price: 15,
+	},
+];
+
+function Home() {
+	return (
+		<div>
+			<h1>🍕 Fast React Pizza Co.</h1>
+			<p>This page has been rendered with React on the server 🤯</p>
+
+			<h2>Menu</h2>
+			<ul>
+				{pizzas.map((pizza) => (
+					<MenuItem pizza={pizza} key={pizza.name} />
+				))}
+			</ul>
+		</div>
+	);
+}
+
+function Counter() {
+	const [count, setCount] = React.useState(0);
+	return (
+		<div>
+			<button onClick={() => setCount((c) => c + 1)}>+1</button>
+			<span>{count}</span>
+		</div>
+	);
+}
+
+function MenuItem({ pizza }) {
+	return (
+		<li>
+			<h4>
+				{pizza.name} (${pizza.price})
+			</h4>
+			<Counter />
+		</li>
+	);
+}
 
 const server = createServer((req, res) => {
-	const pathname = parse(req.url, true).pathname;
+	const pathName = parse(req.url, true).pathname;
 
-	res.end("Hello world");
+	const htmlTemplate = readFileSync(`${__dirname}/index.html`, "utf-8");
+
+	if (pathName === "/") {
+		const renderedHtml = renderToString(<Home />);
+		const html = htmlTemplate.replace("%%%CONTENT%%%", renderedHtml);
+		res.writeHead(200, { "Content-type": "text/html" });
+		res.end(renderedHtml);
+	} else if (pathName === "/test") {
+		res.end("Test");
+	} else {
+		res.end("The url cannot be found");
+	}
 });
 
 server.listen(8000, () => console.log("Listening requests on port 8000"));
